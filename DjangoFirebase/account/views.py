b3fbf2468206f142f2eb211f11e5307fff12b134
Tedhoon <gt0305@likelion.org>
2020-04-04 19:44:25 +0900

    [add] firebase에서 authentication user 생성 후 통신 테스트

--- DjangoFirebase/account/views.py
+++ DjangoFirebase/account/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+# import pyrebase
+from firebase import firebase as fb
+
+firebaseConfig={
+            'apiKey' : "AIzaSyB0noCCCErBRiQGrK8o0NrDVN9GSRjVOXQ",
+            'authDomain' : "fir-djangouser.firebaseapp.com",
+            'databaseURL' : "https://fir-djangouser.firebaseio.com",
+            'projectId' : "fir-djangouser",
+            'storageBucket' : "fir-djangouser.appspot.com",
+            'messagingSenderId' : "227062425443",
+            'appId' : "1:227062425443:web:23ccd4e33d1560d33d9556"
+            };
+
+firebase = fb.initialize_app(firebaseConfig)
+
+auth=firebase.auth()
+
+def signin(request):
+    return render(reqeust,"index.html")
+    
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
